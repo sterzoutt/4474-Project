5c0f820b60ca6d764c30a01647f1a4f44dba2a46
sterzoutt <your-email@sterzoutt.com>
2026-03-18 16:11:39 -0400

    Initial commit - React game project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Your Game</h1>
+      <p>Add your game code here</p>
+    </div>
+  )
+}
+
+export default App
